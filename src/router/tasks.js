aaad493e7d6dc57773e86e8d33156910a9d623d4
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-02 15:50:08 +0300

    REST API with node

--- src/router/tasks.js
+++ src/router/tasks.js
@@ -1,61 +1,119 @@
-var express = require("express");
-const router = express.Router();
-const Task = require("../models/task");
-//create a post request for task
-router.post("/tasks", async(req, res) => {
-    var task = new Task(req.body);
+var express = require('express')
+var Task = require('../models/task');
+const auth = require('../middleware/auth');
+var router = new express.Router();
+
+// Create a post request 
+// description, completed
+// '/tasks'
+router.post('/tasks', auth, async(req, res) => {
+    var task = new Task({
+        ...req.body,
+        owner: req.user._id
+    });
+
     try {
         await task.save();
         res.status(201).send(task);
-    } catch (e) {
-        res.status(400).send(e);
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
 
-//get request for tasks using async
-router.get("/tasks", async(req, res) => {
+// get all tasks
+// 'get request'
+// '/tasks'
+// '/tasks?completed=true'
+// '/tasks?limit=2'
+// '/tasks?skip=2'
+// '/tasks?sortBy=createdAt:desc'
+router.get('/tasks', auth, async(req, res) => {
+    var match = {}
+    var sort = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        var parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
-        var task = await Task.find({});
-        res.send(task);
-    } catch (e) {
-        res.status(400).send(e);
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate();
+        res.send(req.user.tasks);
+    } catch (err) {
+        res.status(500).send(err);
     }
 });
-//update task
-router.patch("/tasks/:id", async(req, res) => {
-    var updates = Object.keys(req.body);
-    var allowedUpdates = ["description", "completed"];
-    var isValidOperation = updates.every((update) =>
-        allowedUpdates.includes(update)
-    );
-    if (!isValidOperation) {
-        return res.status(400).send({ error: "invalid data input" });
-    }
+
+// get a single task ('/tasks/:id')
+router.get('/tasks/:id', auth, async(req, res) => {
+    var id = req.params.id;
+
     try {
-        var task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
+        var task = await Task.findOne({ id, owner: req.user._id });
         if (!task) {
             return res.status(404).send();
         }
         res.send(task);
-    } catch (e) {
-        res.status(400).send(e);
+    } catch (err) {
+        res.status(500).send(err);
     }
 });
-//delete task
-router.delete("/tasks/:id", async(req, res) => {
-    var id = req.params.id;
+
+// update task
+router.patch('/tasks/:id', auth, async(req, res) => {
+    var updates = Object.keys(req.body);
+    var allowedUpdates = ['description', 'completed'];
+    var isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid data input' });
+    }
+
     try {
-        var task = await Task.findByIdAndDelete(id);
+        var task = await Task.findOne({ id: req.params.id, owner: req.user._id });
+
         if (!task) {
-            res.status(404).send();
+            return res.status(404).send();
+        }
+        updates.forEach((update) => task[update] = req.body[update]);
+        await task.save()
+        res.send(task);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+// delete a task (findByIdAndDelete) ('/tasks/:id')
+// use async wait to do this delete path
+router.delete('/tasks/:id', auth, async(req, res) => {
+    try {
+        var task = await Task.findOneAndDelete({ id: req.params.id, owner: req.user._id });
+        if (!task) {
+            return res.status(404).send();
         }
         res.send(task);
-    } catch (e) {
-        res.status(500).send(e);
+    } catch (err) {
+        res.status(500).send(err);
     }
 });
 
 module.exports = router;
+
+
+// data struts // notesapp
+
+// apis // budget (front-end, backend, deployment)
+
+// react, redux, testing(jest, enzyme), express, heroku, sass, firebase, oauth
