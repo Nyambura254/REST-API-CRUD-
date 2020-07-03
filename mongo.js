02583eebca283a439d58126ed7b954c6fa295a93
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-04 00:08:03 +0300

    REST API with node

--- mongo.js
+++ mongo.js
@@ -4,39 +4,35 @@ var MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb://127.0.0.1:27017';
 var databaseName = 'task-manager';
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, function(err, client) {
-    if (err) {
-        return console.log('unable to connect');
-    }
-    var db = client.db(databaseName);
-    // db.collection('users').insertMany([{
-    //     name: 'jayden',
-    //     age: 36,
-    // }], (err, result) => {
-    //     if (err) {
-    //         return console.log('unable to connect');
-    //     }
-    //     console.log(result.ops);
-    // })
-    db.collection('tasks').insertMany([{
+if (err) {
+return console.log('unable to connect');
+}
+var db = client.db(databaseName); //look at our database name
+// db.collection('users').insertMany([{
+//     name: 'jayden',
+//     age: 36,
+// }], (err, result) => {
+//     if (err) {
+//         return console.log('unable to connect');
+//     }Woman1984
+description: 'coding',
+competed: false,
 
-        description: 'coding',
-        competed: false,
+}, {
 
-    }, {
+description: 'playing',
+competed: false,
 
-        description: 'playing',
-        competed: false,
+}, {
 
-    }, {
+description: 'dancing',
+competed: true,
 
-        description: 'dancing',
-        competed: true,
-
-    }], (err, result) => {
-        if (err) {
-            return console.log('unable to connect');
-        }
-        console.log(result.ops);
-    })
+}], (err, result) => {
+if (err) {
+return console.log('unable to connect');
+}
+console.log(result.ops);
+})
 
 });
