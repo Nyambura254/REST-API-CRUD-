850c7e1e2a1acf4418d322b2f44850a55bf40cbb
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-03 10:23:55 +0300

    REST API with node

--- src/testdata/promises.js
+++ src/testdata/promises.js
@@ -2,7 +2,7 @@
 require('../db/mongoose');
 var Task = require('../models/task');
 //find task by id
-Task.findById('5ef461db433ffc162c3bc168').then((task) => {
+Task.findById('5ef461db433ffc162c3bc168').then((task) => { //we pass that id
     console.log(task);
 }).catch((err) => {
     console.log(err);
