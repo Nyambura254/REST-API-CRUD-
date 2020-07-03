555e141a4b4c9accef6b0994d0239a8f72a76f0d
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-03 10:22:49 +0300

    REST API with node

--- src/testdata/asyncawait.js
+++ src/testdata/asyncawait.js
@@ -2,12 +2,12 @@
 require('../db/mongoose');
 var Task = require('../models/task');
 
-//find task by id
+//find task by id in async
 var findTask = async(id) => {
-    var task = await Task.findById(id);
-    return task;
-}
-
+        var task = await Task.findById(id);
+        return task;
+    }
+    //we pass that id
 findTask('5ef461db433ffc162c3bc168').then((task) => {
     console.log(task);
 }).catch((err) => {
